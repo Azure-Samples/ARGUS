6e78700f2e641ea259a4a55475f988341c77275c
Konstantinos Mavrodis <kmavrodis@microsoft.com>
2024-06-12 11:34:03 +0200

    Added feedback functionality on frontend

--- frontend/explore_data.py
+++ frontend/explore_data.py
@@ -1,27 +1,19 @@
 import sys
 import base64
-import os
 from datetime import datetime
 from azure.storage.blob import BlobServiceClient
 from azure.cosmos import CosmosClient
 import streamlit as st
 import pandas as pd
 from streamlit_pdf_viewer import pdf_viewer
-
-# explore_data.py
-
-
 import plotly.express as px
 import plotly.graph_objects as go
-
 
 def format_finished(finished, error):
     return '✅' if finished else '❌' if error else '➖'
 
-
 def refresh_data():
     return fetch_data_from_cosmosdb(st.session_state.cosmos_documents_container_name)
-
 
 def fetch_data_from_cosmosdb(container_name):
     cosmos_client = CosmosClient(st.session_state.cosmos_url, st.session_state.cosmos_key)
@@ -32,15 +24,12 @@ def fetch_data_from_cosmosdb(container_name):
     items = list(container.query_items(query, enable_cross_partition_query=True))
     return pd.json_normalize(items)
 
-
 def delete_item(dataset_name, file_name, item_id):
-    # Delete the item from CosmosDB
     cosmos_client = CosmosClient(st.session_state.cosmos_url, st.session_state.cosmos_key)
     database = cosmos_client.get_database_client(st.session_state.cosmos_db_name)
     container = database.get_container_client(st.session_state.cosmos_documents_container_name)
     container.delete_item(item=item_id, partition_key={})
 
-    # Delete the file from Blob Storage
     blob_service_client = BlobServiceClient.from_connection_string(st.session_state.blob_conn_str)
     container_client = blob_service_client.get_container_client(st.session_state.container_name)
 
@@ -48,7 +37,6 @@ def delete_item(dataset_name, file_name, item_id):
     blob_client.delete_blob()
 
     st.success(f"Deleted {file_name} from {dataset_name} successfully!")
-
 
 def reprocess_item(dataset_name, file_name):
     blob_service_client = BlobServiceClient.from_connection_string(st.session_state.blob_conn_str)
@@ -58,17 +46,14 @@ def reprocess_item(dataset_name, file_name):
     temp_blob = f"{dataset_name}/{file_name}"
 
     try:
-        # Copy the blob to a temporary location to trigger the processing
         blob_client = container_client.get_blob_client(source_blob)
         temp_blob_client = container_client.get_blob_client(temp_blob)
 
-        # Copy the blob to the same location to trigger re-processing
         temp_blob_client.start_copy_from_url(blob_client.url)
 
         st.success(f"Re-processing triggered for {file_name} in {dataset_name} dataset.")
     except Exception as e:
         st.error(f"Failed to re-process {file_name}: {e}")
-
 
 def fetch_pdf_from_blob(blob_name):
     blob_service_client = BlobServiceClient.from_connection_string(st.session_state.blob_conn_str)
@@ -78,7 +63,6 @@ def fetch_pdf_from_blob(blob_name):
     pdf_data = blob_client.download_blob().readall()
     return pdf_data
 
-
 def fetch_json_from_cosmosdb(item_id):
     cosmos_client = CosmosClient(st.session_state.cosmos_url, st.session_state.cosmos_key)
     database = cosmos_client.get_database_client(st.session_state.cosmos_db_name)
@@ -86,13 +70,32 @@ def fetch_json_from_cosmosdb(item_id):
     item = container.read_item(item=item_id, partition_key={})
     return item
 
+def save_feedback_to_cosmosdb(item_id, rating, comments):
+    cosmos_client = CosmosClient(st.session_state.cosmos_url, st.session_state.cosmos_key)
+    database = cosmos_client.get_database_client(st.session_state.cosmos_db_name)
+    container = database.get_container_client(st.session_state.cosmos_documents_container_name)
+
+    item = container.read_item(item=item_id, partition_key={})
+    if 'feedback' not in item:
+        item['feedback'] = []
+    item['feedback'].append({'timestamp': datetime.utcnow().isoformat(), 'rating': rating, 'comments': comments})
+    container.upsert_item(item)
+
+def get_existing_feedback(item_id):
+    cosmos_client = CosmosClient(st.session_state.cosmos_url, st.session_state.cosmos_key)
+    database = cosmos_client.get_database_client(st.session_state.cosmos_db_name)
+    container = database.get_container_client(st.session_state.cosmos_documents_container_name)
+
+    item = container.read_item(item=item_id, partition_key={})
+    if 'feedback' in item and item['feedback']:
+        return item['feedback'][-1]  # Return the most recent feedback
+    return None
 
 def explore_data_tab():
     df = refresh_data()
     if not df.empty:
         st.toast('Data fetched successfully!')
 
-        # Extract and format relevant fields
         extracted_data = []
         for item in df.to_dict(orient='records'):
             blob_name = item.get('properties.blob_name', '')
@@ -115,20 +118,14 @@ def explore_data_tab():
             extracted_data.append(extracted_item)
 
         extracted_df = pd.DataFrame(extracted_data)
-
-        # Add a selection column
         extracted_df.insert(0, 'Select', False)
-
-        # Sort by request date, most recent first
         extracted_df = extracted_df.sort_values(by='Request Timestamp', ascending=False)
 
-        # Filter options
         st.header("Explore Data")
         filter_col1, filter_col2, filter_col3 = st.columns([3, 1, 1])
 
         with filter_col1:
-            filter_dataset = st.multiselect("Dataset", options=extracted_df['Dataset'].unique(),
-                                            default=extracted_df['Dataset'].unique())
+            filter_dataset = st.multiselect("Dataset", options=extracted_df['Dataset'].unique(), default=extracted_df['Dataset'].unique())
 
         with filter_col2:
             filter_finished = st.selectbox("Processing Status", options=['All', 'Finished', 'Not Finished'], index=0)
@@ -136,34 +133,23 @@ def explore_data_tab():
         with filter_col3:
             filter_date_range = st.date_input("Request Date Range", [])
 
-        # Apply filters
         filtered_df = extracted_df[
             extracted_df['Dataset'].isin(filter_dataset) &
-            (extracted_df['Finished'].apply(
-                lambda x: True if filter_finished == 'All' else (x == '✅' if filter_finished == 'Finished' else
-                                                                  (x == '❌' or x == '➖')))) &
-            (extracted_df['Request Timestamp'].apply(
-                lambda x: (not filter_date_range) or (
-                        x.date() >= filter_date_range[0] and x.date() <= filter_date_range[1])))
+            (extracted_df['Finished'].apply(lambda x: True if filter_finished == 'All' else (x == '✅' if filter_finished == 'Finished' else (x == '❌' or x == '➖')))) &
+            (extracted_df['Request Timestamp'].apply(lambda x: (not filter_date_range) or (x.date() >= filter_date_range[0] and x.date() <= filter_date_range[1])))
         ]
 
         cols = st.columns([0.5, 10, 0.5])
         with cols[1]:
-            # table emoji 
-            
             tabs_ = st.tabs(["🧮 Table", "📐 Analytics"])
 
             with tabs_[0]:
-                # Display the table with selection checkboxes
                 edited_df = st.data_editor(filtered_df, column_config={"id": None})
-
-                # Find selected rows
                 selected_rows = edited_df[edited_df['Select'] == True]
 
-                sub_col = st.columns([1, 1, 1, 5])
+                sub_col = st.columns([1, 1, 1, 3])
 
                 with sub_col[0]:
-                    # Refresh Table button should be below the table
                     if st.button('Refresh Table', key='refresh_table'):
                         df = refresh_data()
 
@@ -178,53 +164,70 @@ def explore_data_tab():
                             reprocess_item(row['Dataset'], row['File Name'])
 
                 if len(selected_rows) == 1:
-                    with st.expander("Show/Hide raw PDF and extracted JSON...", expanded=False):
-                        selected_item = selected_rows.iloc[0]
-                        pdf_blob_name = f"{selected_item['Dataset']}/{selected_item['File Name']}"
-                        json_item_id = selected_item['id']
+                    st.markdown("---")
+                    ## markdown text with selected item name
+                    st.markdown(f"###### {selected_rows.iloc[0]['File Name']}")
 
-                        pdf_data = fetch_pdf_from_blob(pdf_blob_name)
-                        with st.spinner('Fetching PDF and JSON data...'):
-                            if pdf_data:
-                                st.success('PDF fetched successfully!')
-                            else:
-                                st.error('Failed to fetch PDF data.')
+                    selected_item = selected_rows.iloc[0]
+                    pdf_blob_name = f"{selected_item['Dataset']}/{selected_item['File Name']}"
+                    json_item_id = selected_item['id']
+                    
+                    with st.expander("Human in the loop Feedback"):
+                        feedback = get_existing_feedback(json_item_id)
+                        initial_rating = feedback['rating'] if feedback else None
+                        initial_comments = feedback['comments'] if feedback else ""
 
-                            json_data = fetch_json_from_cosmosdb(json_item_id)
-                            if json_data:
-                                st.success('JSON data fetched successfully!')
-                            else:
-                                st.error('Failed to fetch JSON data.')
-                        pdf_col, json_col = st.columns(2)
-                        with pdf_col:
-                            if pdf_data:
-                                # Display the PDF
-                                if sys.getsizeof(pdf_data) > 1500000:
-                                    st.toast('PDF file is too large to display in iframe.')
-                                    download_link = f'<a href="data:application/octet-stream;base64,{base64.b64encode(pdf_data).decode("utf-8")}" download="{pdf_blob_name.split("/")[-1]}">Download PDF</a>'
-                                    pdf_viewer(pdf_data, height=1200)
-                                    st.markdown(download_link, unsafe_allow_html=True)
-                                pdf_base64 = base64.b64encode(pdf_data).decode('utf-8')
-                                pdf_display = f'<iframe src="data:application/pdf;base64,{pdf_base64}" width="100%" height="1200" type="application/pdf"></iframe>'
-                                st.markdown(pdf_display, unsafe_allow_html=True)
+                        # Feedback section with rating and comments
+                        feedback_col1, feedback_col2 = st.columns([1, 2])
+                        with feedback_col1:
+                            rating = st.slider("Extraction Quality", 1, 10, initial_rating, key="rating")
+                        with feedback_col2:
+                            comments = st.text_area("Comments on the Extraction", initial_comments, key="comments")
 
-                        with json_col:
-                            if json_data:
-                                # Display the JSON data
-                                st.json(json_data['extracted_data']['gpt_extraction_output'])
+                        if st.button("Done"):
+                            save_feedback_to_cosmosdb(json_item_id, rating, comments)
+                            st.success("Feedback submitted!")
+
+                    pdf_data = fetch_pdf_from_blob(pdf_blob_name)
+                    with st.spinner('Fetching PDF and JSON data...'):
+                        if pdf_data:
+                            st.toast('PDF fetched successfully!')
+                        else:
+                            st.error('Failed to fetch PDF data.')
+
+                        json_data = fetch_json_from_cosmosdb(json_item_id)
+                        if json_data:
+                            st.toast('JSON data fetched successfully!')
+                        else:
+                            st.error('Failed to fetch JSON data.')
+
+                    pdf_col, json_col = st.columns(2)
+                    with pdf_col:
+                        if pdf_data:
+                            if sys.getsizeof(pdf_data) > 1500000:
+                                st.toast('PDF file is too large to display in iframe.')
+                                download_link = f'<a href="data:application/octet-stream;base64,{base64.b64encode(pdf_data).decode("utf-8")}" download="{pdf_blob_name.split("/")[-1]}">Download PDF</a>'
+                                pdf_viewer(pdf_data, height=1200)
+                                st.markdown(download_link, unsafe_allow_html=True)
+                            pdf_base64 = base64.b64encode(pdf_data).decode('utf-8')
+                            pdf_display = f'<iframe src="data:application/pdf;base64,{pdf_base64}" width="100%" height="1200" type="application/pdf"></iframe>'
+                            st.markdown(pdf_display, unsafe_allow_html=True)
+
+                    with json_col:
+                        if json_data:
+                            st.json(json_data['extracted_data']['gpt_extraction_output'])
+
                 elif len(selected_rows) > 1:
                     st.warning('Please select exactly one item to show extraction.')
 
             with tabs_[1]:
-                col1, col2 = st.columns(2)  # Create two columns for the layout
+                col1, col2 = st.columns(2)
 
                 with col1:
-                    # Pie Chart for Processing Status
                     try:
                         success_counts = filtered_df['Finished'].value_counts()
                         labels = ['Successful', 'Processing', 'Not Successful']
-                        sizes = [success_counts.get('✅', 0), success_counts.get('➖', 0),
-                                 success_counts.get('❌', 0)]
+                        sizes = [success_counts.get('✅', 0), success_counts.get('➖', 0), success_counts.get('❌', 0)]
                         colors = ['green', 'orange', 'red']
 
                         fig3 = go.Figure(data=[go.Pie(labels=labels, values=sizes, marker=dict(colors=colors))])
@@ -236,60 +239,46 @@ def explore_data_tab():
 
                 with col2:
                     try:
-                        fig1 = px.histogram(filtered_df, x='Dataset', title='Number of Files per Dataset',
-                                            labels={'x': 'Dataset', 'y': 'Number of Files'})
+                        fig1 = px.histogram(filtered_df, x='Dataset', title='Number of Files per Dataset', labels={'x': 'Dataset', 'y': 'Number of Files'})
                         fig1.update_layout(xaxis_title_text='Dataset', yaxis_title_text='Number of Files')
                         st.plotly_chart(fig1)
                     except Exception as e:
                         st.error(f"Error in creating the histogram: {e}")
 
-                col3, col4 = st.columns([1, 1])  # Create a new layout row
+                col3, col4 = st.columns([1, 1])
 
                 with col3:
-                    # Histogram of Processing Time
                     try:
-                        fig2 = px.histogram(filtered_df, x='Total Time', nbins=20,
-                                            title='Distribution of Processing Time',
-                                            labels={'x': 'Processing Time (seconds)', 'y': 'Number of Files'})
-                        fig2.update_layout(xaxis_title_text='Processing Time (seconds)',
-                                           yaxis_title_text='Number of Files')
+                        fig2 = px.histogram(filtered_df, x='Total Time', nbins=20, title='Distribution of Processing Time', labels={'x': 'Processing Time (seconds)', 'y': 'Number of Files'})
+                        fig2.update_layout(xaxis_title_text='Processing Time (seconds)', yaxis_title_text='Number of Files')
                         st.plotly_chart(fig2)
                     except Exception as e:
                         st.error(f"Error in creating the histogram: {e}")
 
                 with col4:
-                    # Scatter Plot for Processing Time vs. File Size
                     try:
-                        fig5 = px.scatter(filtered_df, x='Size', y='Total Time',
-                                           title='Processing Time vs. File Size',
-                                           labels={'x': 'File Size (bytes)', 'y': 'Processing Time (seconds)'})
-                        fig5.update_layout(xaxis_title_text='File Size (bytes)',
-                                           yaxis_title_text='Processing Time (seconds)')
+                        fig5 = px.scatter(filtered_df, x='Size', y='Total Time', title='Processing Time vs. File Size', labels={'x': 'File Size (bytes)', 'y': 'Processing Time (seconds)'})
+                        fig5.update_layout(xaxis_title_text='File Size (bytes)', yaxis_title_text='Processing Time (seconds)')
                         st.plotly_chart(fig5)
                     except Exception as e:
                         st.error(f"Error in creating the scatter plot: {e}")
 
                 col5, col6 = st.columns([1, 1])
                 with col5:
-                    # Processing time devided per page by request timestamp (excluding files with 0 pages)
                     try:
-                        fig4 = px.scatter(filtered_df[filtered_df['Pages'] > 0], x='Request Timestamp', y='Total Time',
-                                           color='Pages', title='Processing Time per Page by Request Timestamp',
-                                           labels={'x': 'Request Timestamp', 'y': 'Processing Time (seconds)'})
-                        fig4.update_layout(xaxis_title_text='Request Timestamp',
-                                           yaxis_title_text='Processing Time (seconds)')
+                        fig4 = px.scatter(filtered_df[filtered_df['Pages'] > 0], x='Request Timestamp', y='Total Time', color='Pages', title='Processing Time per Page by Request Timestamp', labels={'x': 'Request Timestamp', 'y': 'Processing Time (seconds)'})
+                        fig4.update_layout(xaxis_title_text='Request Timestamp', yaxis_title_text='Processing Time (seconds)')
                         st.plotly_chart(fig4)
                     except Exception as e:
                         st.error(f"Error in creating the scatter plot: {e}")
                 with col6:
                     try:
-                        fig6 = px.histogram(filtered_df, x='Pages', title='Number of Pages per File',
-                                            labels={'x': 'Number of Pages', 'y': 'Number of Files'})
+                        fig6 = px.histogram(filtered_df, x='Pages', title='Number of Pages per File', labels={'x': 'Number of Pages', 'y': 'Number of Files'})
                         fig6.update_layout(xaxis_title_text='Number of Pages', yaxis_title_text='Number of Files')
                         st.plotly_chart(fig6)
                     except Exception as e:
                         st.error(f"Error in creating the histogram: {e}")
 
     else:
-        st.error(
-            'Failed to fetch data or no data found. If you submitted files for processing, please wait a few seconds and refresh the page.')
+        st.error('Failed to fetch data or no data found. If you submitted files for processing, please wait a few seconds and refresh the page.')
+
